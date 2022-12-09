4e0feb9b86c6f2de61aaf359669dca6f8d968004
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-12-09 14:53:42 +0100

    Checkstyle fixed, request modified

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/domain/RequestRepository.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/domain/RequestRepository.java
@@ -1,8 +1,9 @@
 package nl.tudelft.sem.template.example.domain;
 
+import nl.tudelft.sem.template.example.requests.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import nl.tudelft.sem.template.example.requests.Request;
+
 
 @Repository
 interface RequestRepository extends JpaRepository<Request, Long> {
--- user-microservice/src/main/java/nl/tudelft/sem/template/example/requests/Request.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/requests/Request.java
@@ -27,27 +27,104 @@ public class Request {
     private transient double gpu;
     private transient double mem;
     private transient Date date;
+    private transient String condition;
 
     /**
-     * Constructor for Request class, that is sent by  user
-     *  and will be stored in database.
+     * Constructor for Request class, that is sent by  user and will be stored in database.
      *
-     * @param user - netId of user
+     * @param user        - netId of user
      * @param description - description of request
-     * @param cpu - amount of cpu power requested
-     * @param gpu - amount of gpu power requested
-     * @param mem - amount of memory requested
-     * @param date - due date/deadline
+     * @param cpu         - amount of cpu power requested
+     * @param gpu         - amount of gpu power requested
+     * @param mem         - amount of memory requested
+     * @param date        - due date/deadline
      */
-    public Request(String user, String description, double cpu, double gpu, double mem, Date date) {
+    public Request(String user, String description, double cpu, double gpu, double mem, Date date, String condition) {
         this.user = user;
         this.description = description;
         this.cpu = cpu;
         this.gpu = gpu;
         this.mem = mem;
         this.date = date;
+        this.condition = condition;
     }
 
-    public Request(){}
+    /**
+     * Empty constructor for JPA repository.
+     */
+    public Request() {
+    }
 
+    /**
+     * Getter.
+     *
+     * @return user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return user
+     */
+    public String getCondition() {
+        return condition;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return CPU
+     */
+    public double getCpu() {
+        return cpu;
+    }
+
+    /**
+     * Getter GPU.
+     *
+     * @return gpu
+     */
+    public double getGpu() {
+        return gpu;
+    }
+
+    /**
+     * Getter.
+     *
+     * @return memory
+     */
+    public double getMem() {
+        return mem;
+    }
+    
 }
