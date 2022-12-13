19c2eb6a345f6c06bce101f2f52a4d19b182c35a
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-12-13 15:31:04 +0100

    Tests for controller added

--- user-microservice/src/test/java/nl/tudelft/sem/template/example/controller/AuthenticationControllerTest.java
+++ user-microservice/src/test/java/nl/tudelft/sem/template/example/controller/AuthenticationControllerTest.java
@@ -1,20 +1,24 @@
 package nl.tudelft.sem.template.example.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.tudelft.sem.common.models.request.waitinglist.RequestModel;
 import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationInterface;
 import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationRequestModel;
+import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationResponseModel;
 import nl.tudelft.sem.template.example.feigninterfaces.RegistrationRequestModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,25 +26,48 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles({"test", "mockAuthMicroservice"})
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles({"test"})
 public class AuthenticationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private AuthenticationInterface authenticationInterface;
 
     @Test
-    public void testRegister() throws Exception {
-    //        RegistrationRequestModel registr = new RegistrationRequestModel();
-    //        ResponseEntity val = new ResponseEntity<>("Here", HttpStatus.OK);;
-    //        when(authenticationInterface.register(any())).thenReturn(val);
-    //        ObjectMapper objectMapper = new ObjectMapper();
-    //        String serialisedRegister = objectMapper.writeValueAsString(registr);
-    //        mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
-    //            .content(serialisedRegister)).andExpect(status().isOk()).andExpect(content().string("Here"));
+    public void testRegister() {
+        try {
+            RegistrationRequestModel registr = new RegistrationRequestModel();
+            ResponseEntity val = new ResponseEntity<>("Here", HttpStatus.OK);
+            ;
+            when(authenticationInterface.register(any())).thenReturn(val);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String serialisedRegister = objectMapper.writeValueAsString(registr);
+            mockMvc.perform(post("/login/register").contentType(MediaType.APPLICATION_JSON)
+                .content(serialisedRegister)).andExpect(status().isOk()).andExpect(content().string("Here"));
+        } catch (Exception e) {
+            assertEquals(1, 0);
+        }
+    }
+
+    @Test
+    public void testAuthenticate() {
+        try {
+            AuthenticationRequestModel request = new AuthenticationRequestModel();
+            AuthenticationResponseModel response = new AuthenticationResponseModel();
+            response.setToken("123");
+            ResponseEntity val = new ResponseEntity<AuthenticationResponseModel>(response, HttpStatus.OK);
+            ;
+            when(authenticationInterface.authenticate(any())).thenReturn(val);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String serialisedRegister = objectMapper.writeValueAsString(request);
+            mockMvc.perform(post("/login/authenticate").contentType(MediaType.APPLICATION_JSON)
+                    .content(serialisedRegister)).andExpect(status().isOk()).andExpect(jsonPath("token").exists())
+                .andExpect(jsonPath("token").value("123"));
+        } catch (Exception e) {
+            assertEquals(1, 0);
+        }
     }
 }
