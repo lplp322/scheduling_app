1cbbf5185de5edd2649ed84eecfaf2ec6464f341
Radu Ciobanu <R.I.Ciobanu@student.tudelft.nl>
2022-12-13 23:18:14 +0100

    small changes

--- common/src/main/java/nl/tudelft/sem/common/authentication/JwtTokenUtils.java
+++ common/src/main/java/nl/tudelft/sem/common/authentication/JwtTokenUtils.java
@@ -13,9 +13,33 @@ public class JwtTokenUtils {
      * @param roles collection of roles to be checked
      * @return true if every element in roles is present in token's roles
      */
-    public static boolean isTokenValid(String token, Collection<String> roles) {
+    public static boolean checkTokenRoles(String token, Collection<String> roles) {
         Claims claims = Jwts.parser()
-                .setSigningKey("secret")
+                .setSigningKey("exampleSecret")
+                .parseClaimsJws(token)
+                .getBody();
+
+        List<String> tokenRoles = claims.get("roles", List.class);
+
+        for (String i : roles) {
+            if (!tokenRoles.contains(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /** Checks whether a token has all the roles passed to it or not.
+     *
+     * @param token token to check
+     * @param roles collection of roles to be checked
+     * @param secret secret encryption/decryption key
+     * @return true if every element in roles is present in token's roles
+     */
+    public static boolean checkTokenRoles(String token, Collection<String> roles, String secret) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secret)
                 .parseClaimsJws(token)
                 .getBody();
 
---
+++ common/src/test/java/nl/tudelft/sem/common/authentication/JwtTokenUtilsTest.java
@@ -0,0 +1,15 @@
+package nl.tudelft.sem.common.authentication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtTokenUtilsTest {
+    @Test
+    void checkTokenRoles() {
+    }
+
+    @Test
+    void testCheckTokenRoles() {
+    }
+}
