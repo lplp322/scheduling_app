b202941ef6a1b9d60baa119de4f5332f9379cc41
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-12-22 16:46:30 +0100

    Pipeline fix

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/requestmodelget/RequestModelCreatorStrategy.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/requestmodelget/RequestModelCreatorStrategy.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import nl.tudelft.sem.common.models.request.RequestModelWaitingList;
 
 /**
- * Interface that is used to create different strategies 
+ * Interface that is used to create different strategies.
  */
 public interface RequestModelCreatorStrategy {
     RequestModelWaitingList createRequestModel(HttpServletRequest httpRequest) throws Exception;
