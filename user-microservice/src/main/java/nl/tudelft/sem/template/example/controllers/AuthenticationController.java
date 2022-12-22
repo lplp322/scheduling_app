d18afa6e66e8357e4bff089a964ca0cf24a3beb6
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2022-12-22 13:52:03 +0100

    Fixed unauthorized

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/AuthenticationController.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/AuthenticationController.java
@@ -1,17 +1,20 @@
 package nl.tudelft.sem.template.example.controllers;
 
 
+import feign.FeignException;
 import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationInterface;
 import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationRequestModel;
 import nl.tudelft.sem.template.example.feigninterfaces.AuthenticationResponseModel;
 import nl.tudelft.sem.template.example.feigninterfaces.RegistrationRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/login")
@@ -24,11 +27,19 @@ public class AuthenticationController {
      */
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody RegistrationRequestModel request) {
-        return authMicroservice.register(request);
+        try {
+            return authMicroservice.register(request);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponseModel> authenticate(@RequestBody AuthenticationRequestModel request) {
-        return authMicroservice.authenticate(request);
+    public ResponseEntity authenticate(@RequestBody AuthenticationRequestModel request) {
+        try {
+            return authMicroservice.authenticate(request);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage(), e);
+        }
     }
 }
